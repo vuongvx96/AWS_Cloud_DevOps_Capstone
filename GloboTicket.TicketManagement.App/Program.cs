957818b9c837a080f0f4408c3bfc19eeff718b7b
Vuong Vo <xuanvuong301@gmail.com>
2022-05-26 01:15:47 +0700

    first commit

---
+++ GloboTicket.TicketManagement.App/Program.cs
@@ -0,0 +1,34 @@
+using Blazored.LocalStorage;
+using GloboTicket.TicketManagement.App;
+using GloboTicket.TicketManagement.App.Auth;
+using GloboTicket.TicketManagement.App.Contracts;
+using GloboTicket.TicketManagement.App.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Reflection;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+builder.Services.AddSingleton(new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:5001"),
+    DefaultRequestVersion = new Version(2, 0)
+}); ;
+
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:5001"));
+
+builder.Services.AddScoped<IEventDataService, EventDataService>();
+builder.Services.AddScoped<ICategoryDataService, CategoryDataService>();
+builder.Services.AddScoped<IOrderDataService, OrderDataService>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+await builder.Build().RunAsync();
